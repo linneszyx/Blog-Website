b9cd749eee90030095390cb1434b8ab92903d473
linneszyx <itssuryanshcooldude346@gmail.com>
2023-03-04 18:04:31 +0530

    final

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const ejs = require("ejs");
+const _ = require("lodash");
 const homeStartingContent =
   "This is a blog website. You can write your own blogs and read other's blogs.";
 const aboutContent =
@@ -8,11 +9,13 @@ const aboutContent =
 const contactContent =
   "Contact us : If you have any questions or would like to get in touch with us, please fill out the form below. We will respond as soon as possible. Thank you!";
 const port = 3000;
+const posts = [];
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
-  res.render("home", { homeStartingContent: homeStartingContent });
+  res.render("home", { homeStartingContent: homeStartingContent, 
+                        posts: posts });
 });
 app.get("/about", (req, res) => {
   res.render("about", { aboutContent: aboutContent });
@@ -24,6 +27,20 @@ app.get("/compose", (req, res) => {
     res.render("compose");
 });
 app.post("/compose", (req, res) => {
-    console.log(req.body.postTitle);
+    const post = {
+        title: req.body.postTitle,
+        content: req.body.postBody
+    }
+    posts.push(post);
+    res.redirect("/");
+});
+app.get("/posts/:postName", (req, res) => {
+    const requestedTitle = _.lowerCase(req.params.postName);
+    posts.forEach(post => {
+        const storedTitle = _.lowerCase(post.title);
+        if (storedTitle === requestedTitle) {
+            res.render("post", { title: post.title, content: post.content });
+        }
+    });
 });
 app.listen(port, () => console.log(`App listening on port ${port}!`));
